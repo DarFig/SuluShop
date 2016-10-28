e64c5882a6a716db0148e1199f297e3d42b258a7
orggue <jorgechato1@gmail.com>
2016-10-28 11:50:20 +0200

    list of products rendered

--- sulushop/views.py
+++ sulushop/views.py
@@ -1,5 +1,7 @@
 from sulushop import app
+from flask import request
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template("_modules/lista.html");
