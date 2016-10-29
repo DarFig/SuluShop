db6ffff23566b12f729ca92b32c91a1579528074
DarFig <BattleDFP@gmail.com>
2016-10-29 14:26:07 +0200

    registro_login, y perfil
    Estructuras y funciones para pasar al login, volver a la lista con el
    formulario, o ir al registro y que el formulario te lleve al perfil

--- sulushop/views.py
+++ sulushop/views.py
@@ -1,7 +1,32 @@
+import json
 from sulushop import app
 from flask import request
-from flask import render_template
+from flask import (render_template, redirect, url_for
+								, request, make_response)
+
 
 @app.route('/')
 def index():
     return render_template("_modules/lista.html");
+	
+
+@app.route('/')
+@app.route('/registro_login.html')
+def regLog():
+	return render_template("_modules/registro_login.html");
+	
+	
+@app.route('/')
+@app.route('/templates/_modules/perfil.html')
+def perfil():
+	return render_template("_modules/perfil.html");
+
+@app.route('/login', methods=['POST'])#todo
+def login():
+	response = make_response( redirect(url_for('index')))
+	return response;	
+	
+@app.route('/registro', methods=['POST'])#todo
+def registro():
+	response = make_response( redirect(url_for('perfil')))
+	return response;	
