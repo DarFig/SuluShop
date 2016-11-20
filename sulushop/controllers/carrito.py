5601b20c30c7b173f30f87a53518e3cd8df48f41
orggue <jorgechato1@gmail.com>
2016-11-20 13:12:23 +0100

    products in cart and mock data

--- sulushop/controllers/carrito.py
+++ sulushop/controllers/carrito.py
@@ -10,12 +10,22 @@ from ..util import *
 @login_required
 def cart(buy=False):
     cur = mysql.connection.cursor()
-    string = '''SELECT * FROM carro WHERE id_usuario = {}
+    string = '''SELECT producto.nombre, SUM(producto.precio * carro.cantidad),
+                        carro.cantidad, producto.id
+                FROM carro
+                LEFT JOIN producto on carro.id_producto = producto.id
+                WHERE carro.id_usuario = {}
+                GROUP BY producto.id
             '''.format(int(get_user_id()))
     cur.execute(string)
     summary = cur.fetchall()
     data = map(list, summary)
 
+    for producto in data:
+        pictures = get_product_cover(producto[3])
+        if pictures:
+            producto.append(pictures)
+
     return render_template('_views/carrito.html', productos=data)
-    # buy = request.args.get('buy', buy)
+# buy = request.args.get('buy', buy)
     # return render_template("_views/carrito.html")
--- sulushop/util.py
+++ sulushop/util.py
@@ -40,3 +40,35 @@ def get_user_id():
     data = map(list, summary)
 
     return data[0][0]
+
+
+def get_product_pictures(pk):
+    cur = mysql.connection.cursor()
+    string = '''SELECT foto.url, foto.principal
+                FROM foto
+                INNER JOIN foto_producto on foto_producto.id_foto = foto.id
+                WHERE foto_producto.id_producto = {}
+                GROUP BY foto.id
+            '''.format(pk)
+    cur.execute(string)
+    photo = cur.fetchall()
+    data = map(list, photo)
+
+    return data
+
+
+def get_product_cover(pk):
+    cur = mysql.connection.cursor()
+    string = '''SELECT foto.url, foto.principal
+                FROM foto
+                INNER JOIN foto_producto on foto_producto.id_foto = foto.id
+                WHERE foto_producto.id_producto = {} AND foto.principal = 1
+                GROUP BY foto.id
+                ORDER BY foto.id DESC
+                LIMIT 1
+            '''.format(pk)
+    cur.execute(string)
+    photo = cur.fetchall()
+    data = map(list, photo)
+
+    return data
